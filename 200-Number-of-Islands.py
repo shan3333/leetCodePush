4df9a160d953fdeb4ebf5c44fbcfd0fe025194e7
Shan <36077030+shan3333@users.noreply.github.com>
2024-10-24 20:12:52 +0100

    [200] [Time Beats: 80.39%] [Memory Beats: 68.05%] - LeetPush

--- 200-Number-of-Islands.py
+++ 200-Number-of-Islands.py
@@ -1,24 +1,24 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
-
         h = len(grid[0])
         v = len(grid)
+        directions = [(-1,0), (1,0), (0,-1), (0,1)]
         islands = 0
-        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-
+        
+        queue = deque()
         for i in range(v):
             for j in range(h):
                 if grid[i][j] == \1\:
+                    queue.append((i,j))
+                    grid[i][j] = \0\
                     islands += 1
-                    queue = deque([(i,j)])
-                    while queue:
-                        x, y = queue.popleft()
-                        if 0<=x<v and 0<=y<h and grid[x][y] == \1\:
-                            grid[x][y] = \0\
-                            for dx, dy in directions:
-                                queue.append([x+dx, y+dy])
+
+                while queue:
+                    x,y = queue.popleft()
+                    for dx, dy in directions:
+                        x_new, y_new = x+dx, y+dy
+                        if grid[x_new][y_new] == \1\:
+                            queue.append((x_new, y_new))
+                            grid[x_new][y_new] = \0\
 
         return islands
-                
