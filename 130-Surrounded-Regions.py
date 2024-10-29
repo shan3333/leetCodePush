d91a9f4e55d9b5c35dae0862a6cdc74020331245
Shan <36077030+shan3333@users.noreply.github.com>
2024-10-29 21:02:00 +0000

    [130] [Time Beats: 79.64%] [Memory Beats: 24.14%] - LeetPush

--- 130-Surrounded-Regions.py
+++ 130-Surrounded-Regions.py
@@ -3,19 +3,22 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        def dfs(i,j):
-            if i<0 or j<0 or i>=len(board) or j>=len(board[0]) or board[i][j]!="O":
-                return
-            board[i][j] = "T"
-            dfs(i-1, j)
-            dfs(i+1, j)
-            dfs(i, j-1)
-            dfs(i, j+1)
-
+        queue = deque()
+        direction = [(1,0),(-1,0),(0,-1),(0,1)]
         for x in range(len(board)):
             for y in range(len(board[0])):
-                if (x==0 or y==0 or x==len(board)-1 or y==len(board[0])-1) and board[x][y]=="O":
-                    dfs(x, y)
+                if board[x][y]=="O" and (x==0 or y==0 or x==len(board)-1 or y==len(board[0])-1):
+                    queue.append((x,y))
+                    
+        while queue:
+            x, y = queue.pop()
+            board[x][y] = "T"
+            for xd, yd in direction:
+                x_new, y_new = x+xd, y+yd
+                if x_new>=0 and y_new>=0 and x_new<len(board) and y_new<len(board[0]) and board[x_new][y_new]=="O":
+                    queue.append((x_new, y_new))
+                        
+
 
         for x in range(len(board)):
             for y in range(len(board[0])):
