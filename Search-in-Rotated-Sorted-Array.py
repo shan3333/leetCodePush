a4fbcfc91915287fbd1940e1b1b92ef73fb8fd97
Shan <36077030+shan3333@users.noreply.github.com>
2025-05-24 13:14:56 +0100

    [33] [Time Beats: 100.00%] [Memory Beats: 66.30%] - LeetPush

--- Search-in-Rotated-Sorted-Array.py
+++ Search-in-Rotated-Sorted-Array.py
@@ -1,21 +1,19 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left = 0
-        right = len(nums) - 1
+        left, right = 0, len(nums)-1
 
         while left<=right:
-            mid = (left + right) // 2
+            mid = (left+right) // 2
             if nums[mid] == target:
                 return mid
-            elif nums[mid] >= nums[left]:
-                if nums[mid] > target and target >= nums[left]:
+            elif nums[left]<=nums[mid]:
+                if target<nums[mid] and target>=nums[left]:
                     right = mid - 1
                 else:
                     left = mid + 1
             else:
-                if nums[mid] < target and target <= nums[right]:
+                if target>nums[mid] and target<=nums[right]:
                     left = mid + 1
                 else:
-                    right = mid -1
-                
+                    right = mid - 1
         return -1
