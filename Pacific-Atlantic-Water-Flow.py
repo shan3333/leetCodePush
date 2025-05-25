c101a4ff6d5f0a1ab7b8bc8f3e8b103ee6b211d0
Shan <36077030+shan3333@users.noreply.github.com>
2025-05-25 17:22:21 +0100

    [417] [Time Beats: 44.93%] [Memory Beats: 30.43%] - LeetPush

--- Pacific-Atlantic-Water-Flow.py
+++ Pacific-Atlantic-Water-Flow.py
@@ -1,38 +1,42 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         m = len(heights) # row
-        n = len(heights[0]) # column
+        n = len(heights[0]) # col
+        res = []
 
         pac = [[False for _ in range(n)] for _ in range(m)]
         atlan = [[False for _ in range(n)] for _ in range(m)]
 
-        dirs = [[0,-1], [0,1], [-1,0], [1,0]]
+        dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]] 
         def get_neighbors(x, y):
             neighbors = []
-            for dir_x, dir_y in dirs:
-                if 0<=x+dir_x<m and 0<=y+dir_y<n:
-                    neighbors.append([x+dir_x, y+dir_y])
+            for dir in dirs:
+                new_x, new_y = x+dir[0], y+dir[1]
+                if 0<=new_x<m and 0<=new_y<n:
+                    neighbors.append([new_x,new_y])
             return neighbors
-
+        
         def dfs(x, y, ocean):
             if ocean[x][y]:
                 return
             ocean[x][y] = True
-            for x_new, y_new in get_neighbors(x,y):
-                if heights[x_new][y_new] >= heights[x][y]:
-                    dfs(x_new, y_new, ocean)
-
+            neighbors = get_neighbors(x,y)
+            for n_x, n_y in neighbors:
+                if heights[n_x][n_y] >= heights[x][y]:
+                    dfs(n_x, n_y, ocean)
+            
         for x in range(m):
             dfs(x, 0, pac)
             dfs(x, n-1, atlan)
-        
+
         for y in range(n):
             dfs(0, y, pac)
             dfs(m-1, y, atlan)
-
-        res = []
+        
         for x in range(m):
             for y in range(n):
                 if pac[x][y] and atlan[x][y]:
                     res.append([x,y])
+        
         return res
+
