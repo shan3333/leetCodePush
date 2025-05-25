294a3ef1e3640acc0e3ad7520bdd5b540a42a28d
Shan <36077030+shan3333@users.noreply.github.com>
2025-05-25 16:41:07 +0100

    [210] [Time Beats: 28.23%] [Memory Beats: 40.29%] - LeetPush

--- Course-Schedule-II.py
+++ Course-Schedule-II.py
@@ -1,28 +1,29 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        visited = [False for _ in range(numCourses)]
+        finished = []
         graph = defaultdict(list)
 
         for cour, pre in prerequisites:
             graph[pre].append(cour)
-
-        finished = []
-        def dfs(pre):
-            for course in graph[pre]:
-                if not visited[course]:
-                    visited[course] = True
-                    if not dfs(course): 
-                        return False
-                elif course not in finished:
-                    return False
-            finished.append(pre)
-            return True
         
+        visited = [False for _ in range(numCourses)]
+        def dfs(cour):
+            for pre in graph[cour]:
+                if not visited[pre]:
+                    visited[pre] = True
+                    if not dfs(pre): 
+                        return False
+                elif pre not in finished:
+                    return False
+
+            finished.append(cour)
+            return True
+
         for i in range(numCourses):
             if not visited[i]:
                 visited[i] = True
-                if not dfs(i): return []
+                if not dfs(i):
+                    return []
         
         finished.reverse()
         return finished
-
