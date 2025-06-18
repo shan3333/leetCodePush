fff46a52e66d165df16250473be8d3eeec5ba859
Shan <36077030+shan3333@users.noreply.github.com>
2025-06-18 18:23:23 +0100

    [1] [Time Beats: 100.00%] [Memory Beats: 50.99%] - LeetPush

--- Two-Sum.py
+++ Two-Sum.py
@@ -1,8 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        l = {}
-        for k, v in enumerate(nums):
-            if target-v in l.keys():
-                return [k, l[target-v]]
-            else:
-                l[v] = k
+        d = {}
+        for k,v in enumerate(nums):
+            if target-v in d:
+                return [k,d[target-v]]
+            d[v] = k
+        
