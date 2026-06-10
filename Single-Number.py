653c3417a6c0b8507be9bf5e26d6fb80db39cd78
Shan <36077030+shan3333@users.noreply.github.com>
2026-06-10 12:28:43 +0200

    [136] [Time Beats: 41.17%] [Memory Beats: 70.07%] - LeetPush

--- Single-Number.py
+++ Single-Number.py
@@ -1,10 +1,7 @@
 1class Solution:
 2    def singleNumber(self, nums: List[int]) -> int:
-3        s = set()
+3        c = 0
 4        for i in nums:
-5            if i not in s:
-6                s.add(i)
-7            else:
-8                s.remove(i)
-9        
-10        return s.pop()
+5            c = c ^ i
+6        
+7        return c
