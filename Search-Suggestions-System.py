1fac83a8fc0b72cc82b205c4fbbe229707f84a35
Shan <36077030+shan3333@users.noreply.github.com>
2026-06-20 11:44:53 +0200

    [1268] [Time Beats: 24.99%] [Memory Beats: 8.62%] - LeetPush

--- Search-Suggestions-System.py
+++ Search-Suggestions-System.py
@@ -1,59 +1,47 @@
 1class TrieNode:
 2    def __init__(self):
 3        self.childNodes = [None] * 26
-4        self.isEnd = False
-5        self.word = None
-6
-7class Trie:
-8    def __init__(self):
-9        self.root = TrieNode()
-10
-11    def insert(self, word):
-12        curr = self.root
-13        for i in word:
-14            index = ord(i) - ord('a')
+4        self.suggestions = []
+5
+6class Trie:
+7    def __init__(self):
+8        self.root = TrieNode()
+9
+10    def insert(self, word):
+11        curr = self.root
+12        for i in word:
+13            index = ord(i) - ord('a')
+14
 15            if not curr.childNodes[index]:
 16                curr.childNodes[index] = TrieNode()
 17            curr = curr.childNodes[index]
-18        curr.word = word
-19        curr.isEnd = True
-20
-21    def startWith(self, searchWord): 
-22        curr = self.root
-23        for i in searchWord:
-24            index = ord(i) - ord('a')
-25            if not curr.childNodes[index]:
-26                return None
-27            curr = curr.childNodes[index]
-28        return curr
-29            
-30
-31class Solution:
-32    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-33        ans = []
-34        trie = Trie()
-35        for p in products:
-36            trie.insert(p)
-37        
-38        def dfs(node, suggestions):
-39            if len(suggestions) == 3:
-40                return
-41            if node.isEnd:
-42                suggestions.append(node.word)
-43            
-44            for child in node.childNodes:
-45                if child:
-46                    dfs(child, suggestions)
-47                if len(suggestions) == 3:
-48                    return
-49        
-50        for i, v in enumerate(searchWord):
-51            s = searchWord[:i+1]
-52            n = trie.startWith(s)
-53
-54            suggestions = []
-55            if n:
-56                dfs(n, suggestions)
-57            ans.append(suggestions)
-58        
-59        return ans
+18
+19            if len(curr.suggestions) < 3:
+20                curr.suggestions.append(word)
+21        
+22    def startWith(self, searchWord):
+23        curr = self.root
+24        for i in searchWord:
+25            index = ord(i) - ord('a')
+26
+27            if not curr.childNodes[index]:
+28                return None
+29            curr = curr.childNodes[index]
+30        return curr
+31
+32class Solution:
+33    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+34        ans = []
+35        trie = Trie()
+36        products.sort()
+37        for p in products:
+38            trie.insert(p)
+39        
+40        for i, v in enumerate(searchWord):
+41            s = searchWord[:i+1]
+42            n = trie.startWith(s)
+43            if n:
+44                ans.append(n.suggestions)
+45            else:
+46                ans.append([])
+47        return ans
