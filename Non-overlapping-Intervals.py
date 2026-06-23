865753c57d5e122adc3356d13dbf103922711b9e
Shan <36077030+shan3333@users.noreply.github.com>
2026-06-23 11:29:56 +0200

    [435] [Time Beats: 64.21%] [Memory Beats: 7.43%] - LeetPush

--- Non-overlapping-Intervals.py
+++ Non-overlapping-Intervals.py
@@ -7,14 +7,9 @@
 7        for i in range(1, len(intervals)):
 8            new_start = intervals[i][0]
 9            new_end = intervals[i][1]
-10            if new_start >= start and new_start < end:
+10            if new_start < end:
 11                ans += 1
-12            elif new_end > start and new_end <= end:
-13                ans += 1
-14            elif new_start <= start and new_end >= end:
-15                ans += 1
-16            else:
-17                start = min(start, new_start)
-18                end = max(end, new_end)
-19
-20        return ans
+12            else:
+13                end = new_end
+14
+15        return ans
